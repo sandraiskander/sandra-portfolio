70788eb116088467d5e44aa0b7ae507b41599444
sandraiskander <sandraiskander3@gmail.com>
2026-09-25 00:30:10 +0300

    updates

--- vite.config.ts
+++ vite.config.ts
@@ -1,13 +1,7 @@
-import tailwindcss from '@tailwindcss/vite'
-import react from '@vitejs/plugin-react'
 import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
 export default defineConfig({
-  base: './',
-  plugins: [
-    tailwindcss(),
-    react(),
-  ],
+  plugins: [react()],
+  base: '/sandra-portfolio/',
 })
-
